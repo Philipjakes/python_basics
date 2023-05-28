52beb608e4ee13e158c6312cccb72079895a69e8
Amin Borhani <105095446+Philipjakes@users.noreply.github.com>
2023-05-28 17:21:11 +0330

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,56 @@
+# PYTHON BUILT-IN DATA TYPES:
+# string:
+# data_type = 'this is tree'
+# print(type(data_type))
+# output: <class 'str'>
+
+# int(integers):
+# intr = 32
+# print(type(intr))
+# output: <class 'int'>
+
+# float:
+# fl = 25.75
+# print(type(fl))
+# output: <class 'float'>
+
+# list:
+# newlist = ["door","tv","chair"]
+# print(type(newlist))
+# output: <class 'list'>
+
+# tuple:
+# tup = ("amy","george",True)
+# print(type(tup))
+# output: <class 'tuple'>
+
+# range:
+# ran = range(6)
+# print(type(ran))
+# output: <class 'range'>
+
+# dictionary:
+# diction = {"name":"john","years":"1989"}
+# print(type(diction))
+# output: <class 'dict'>
+
+# set:
+# sett = {"flower","gold"}
+# print(type(sett))
+# output: <class 'set'>
+
+# boolean:
+# bolean = True
+# print(type(bolean))
+# output: <class 'bool'>
+
+# bytes:
+# by = b"array"
+# print(type(by))
+# output: <class 'bytes'>
+
+# memoryview:
+memory_v = memoryview(bytes(10))
+print(memory_v)
+print(type(memory_v))
+# output: <class 'bytes'>
