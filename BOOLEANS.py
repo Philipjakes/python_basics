d048a60f57e346dc4bec05c7e4690ecdf2cab971
Amin Borhani <105095446+Philipjakes@users.noreply.github.com>
2023-05-28 18:11:39 +0330

    Add files via upload

---
+++ BOOLEANS.py
@@ -0,0 +1,8 @@
+booleans represents True or False,e.g:
+print(1<0.5) 
+output:False
+print(1>0.5)
+# output:True
+point:empty values such as [],{},"",0,None evaluate to False,e.g:
+print(bool(0))
+# output:False
