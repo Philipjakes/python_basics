95261411906cdd87763b0db962c597f0ad2dbc96
Amin Borhani <105095446+Philipjakes@users.noreply.github.com>
2023-05-28 17:36:01 +0330

    Update data type.py

--- data type.py
+++ data type.py
@@ -1,52 +1,52 @@
 # PYTHON BUILT-IN DATA TYPES:
-# string:
-# data_type = 'this is tree'
-# print(type(data_type))
+string:
+data_type = 'this is tree'
+print(type(data_type))
 # output: <class 'str'>
 
 # int(integers):
-# intr = 32
-# print(type(intr))
+intr = 32
+print(type(intr))
 # output: <class 'int'>
 
-# float:
-# fl = 25.75
-# print(type(fl))
+float:
+fl = 25.75
+print(type(fl))
 # output: <class 'float'>
 
 # list:
-# newlist = ["door","tv","chair"]
-# print(type(newlist))
+newlist = ["door","tv","chair"]
+print(type(newlist))
 # output: <class 'list'>
 
 # tuple:
-# tup = ("amy","george",True)
-# print(type(tup))
+tup = ("amy","george",True)
+print(type(tup))
 # output: <class 'tuple'>
 
 # range:
-# ran = range(6)
-# print(type(ran))
+ran = range(6)
+print(type(ran))
 # output: <class 'range'>
 
 # dictionary:
-# diction = {"name":"john","years":"1989"}
-# print(type(diction))
+diction = {"name":"john","years":"1989"}
+print(type(diction))
 # output: <class 'dict'>
 
 # set:
-# sett = {"flower","gold"}
-# print(type(sett))
+sett = {"flower","gold"}
+print(type(sett))
 # output: <class 'set'>
 
 # boolean:
-# bolean = True
-# print(type(bolean))
+bolean = True
+print(type(bolean))
 # output: <class 'bool'>
 
 # bytes:
-# by = b"array"
-# print(type(by))
+by = b"array"
+print(type(by))
 # output: <class 'bytes'>
 
 # memoryview:
