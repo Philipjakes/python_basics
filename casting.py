d205b47b33c64b32c51882318b17bba494f3f605
Amin Borhani <105095446+Philipjakes@users.noreply.github.com>
2023-05-28 17:29:36 +0330

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,12 @@
+casting: it uses to specify a type on to  a variable,e.g:
+a = int(1)
+print(a)
+# output: 1
+b = float(1)
+print(b)
+# output: 1.0
+c = str(1)
+print(c)
+# output: 1
+print(f'type c is :{type(c)}')
+# output: type c is :<class 'str'>
